394640b2932e3cc9522aa439a9aac7be1418143e
juliphy <96592869+juliphy@users.noreply.github.com>
2023-03-18 21:37:19 +0100

    Big update
    Everything just works.

--- index.js
+++ index.js
@@ -5,17 +5,68 @@ const uri = 'mongodb+srv://juliphyy:lJD1NJ04Ilhniw0A@cluster0.vpa0axs.mongodb.ne
 const app = express()
 const client = new MongoClient(uri);
 
-app.get('/api', async function(req,res){
+app.use(express.json(),);
+app.use((req, res, next) => {
+    res.append('Access-Control-Allow-Origin', ['*']);
+    res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.append('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+});
+
+
+app.post('/post', async function(req,res){
+    info = req.body;
+    console.log(info)
+
+    try {
+        await client.connect();
+        const db = client.db("magicdocs");
+        const collection = db.collection("data");
+        const user = info;
+        const result = await collection.insertOne(user);
+    } catch(err) {
+        console.log(err);
+    } finally {
+        await client.close();
+    }
+
+    res.sendStatus(200);
+})
+
+
+app.get('/page', async function(req,res){
     let id = req.query.id;
+    try {
+        await client.connect()
+        let db = client.db('magicdocs')
+        let collection = db.collection('data')
+        let result = await collection.find({id:id}).toArray()
+        res.send(result)
+    } catch (err) {
+        console.log(err)
+    } finally {
+        await client.close();
+        console.log('Connection closed.')
+    }
+})
 
-    await client.connect()
-    let db = client.db('magicdocs')
-    let collection = db.collection('data')
-    let result = await collection.find({id:id}).toArray()
-    let userObject = result[0]
-
-    res.send(JSON.stringify({fullname: userObject.fullname, name:userObject.firstname, birthdate: userObject.birthdate, passport_id: userObject.passport_id})   )
+app.get('/tele', async function(req,res){
+    let chatID = req.query.chatID;
+    try {
+        await client.connect()
+        let db = client.db('magicdocs')
+        let collection = db.collection('data')
+        let result = await collection.find({chatID:chatID}).toArray()
+        res.send(result)
+    } catch (err) {
+        console.log(err)
+    } finally {
+        await client.close();
+        console.log('Connection closed.')
+    }
 })
 
 // {fullname: userObject.fullname, name:userObject.firstname, birthdate: userObject.birthdate, passport_id: userObject.passport_id}
 app.listen(3132)
+
+module.exports = app;
