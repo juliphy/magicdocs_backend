b9294b2da23112a786271659097b15398bd13a58
juliphy <96592869+juliphy@users.noreply.github.com>
2023-05-19 19:06:35 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const {MongoClient} = require('mongodb')
 const express = require('express')
 const uri = 'mongodb+srv://juliphyy:s9EySlqFCKbcpSqY@cluster0.vpa0axs.mongodb.net/?retryWrites=true&w=majority'
-s9EySlqFCKbcpSqY
+
 const app = express()
 const client = new MongoClient(uri);
 
