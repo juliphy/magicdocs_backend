e679db1a05c60512422b83b60a1a512a03b22f26
S. <96592869+juliphy@users.noreply.github.com>
2024-06-15 13:34:50 +0200

    Draw ability commit

--- index.js
+++ index.js
@@ -2,8 +2,11 @@ const {MongoClient} = require('mongodb')
 const express = require('express')
 const uri = 'mongodb+srv://juliphyy:l7jOBx88bEV9kvw5@cluster0.vpa0axs.mongodb.net/?retryWrites=true&w=majority'
 
+const form_data = require('form-data')
+
 const app = express()
 const client = new MongoClient(uri);
+const API_KEY = "d2f5768f8798f57a63d32ddd6a4e9f8e";
 
 app.use(express.json(),);
 app.use((req, res, next) => {
@@ -80,6 +83,47 @@ app.get('/login', async function(req,res){
         console.log('Connection closed.')
     }
 })
+
+app.post('/upload', async (req, res) => {
+    try {
+
+        const id = req.query.id;
+
+        const base64data = req.body.image;
+
+        let form = new FormData();
+
+        form.append('image', base64data)
+
+        const response = await axios.post(
+            'https://api.imgbb.com/1/upload',
+            form,
+            {
+              params: {
+                'key': API_KEY
+              }
+            }
+          );
+
+        const url = response.data.data.url;
+
+        let db = client.db('magicdocs');
+        let collection = db.collection('data');
+
+        const result = collection.updateOne(
+            {id: id},
+            {$set: {
+                "img.urlSign": url
+            }}
+        )
+        
+        res.sendStatus(200)
+          
+    } catch (error) {
+        console.error('Error uploading image:', error);
+        res.status(500).send('Error uploading image');
+    }
+});
 
 
 
